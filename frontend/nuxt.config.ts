de50f2fda9313f9c228465dfad399877a8b7171a
Brubru <jordan.sankare@gmail.com>
2025-11-10 11:37:34 +0100

    fix volume for tailwind

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['./app/assets/css/tailwind.css'],
+  css: ['@/assets/css/tailwind.css'],
   vite: {
     plugins: [
       tailwindcss(),
